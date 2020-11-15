9248777e9078f6cb3afdef36aed2fbd6d9f135a0
Nick Kozhevnikov <info@tocomm.ru>
2020-11-15 18:10:23 +0300

    Simplifying the script

--- delete_old_files.sh
+++ delete_old_files.sh
@@ -1,14 +1,18 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 #Specify path to the archives
-QUANTITY_FILES=`ls /home/<--ENTER PATH--> | wc -l`
+PATH_ARCH="<---ENTER PATH--->"
+
+#Count files in a directory
+QUANTITY_FILES=`ls $PATH_ARCH | wc -l`
 
 #Set the max number of files 
-MAX_FILES=33
+MAX_FILES=4
 
-
-if [ $QUANTITY_FILES \> $MAX_FILES ]
+if [ $QUANTITY_FILES > $MAX_FILES ]
 then
-        cd /home/<--ENTER PATH-->
+        cd $PATH_ARCH
         ls | tail -n+$MAX_FILES | xargs rm -f
 fi
+
+
